e5120620ba9e03a656ea235ef8b1a274f03a8c3c
kajal <25110cn607@glbitm.ac.in>
2026-06-19 22:51:44 +0530

    day14 assignment is uploaded

---
+++ day14/q54.cpp
@@ -0,0 +1,35 @@
+//wap to frequency of an element in an array
+#include <iostream>
+using namespace std;
+
+int frequency(int arr[], int n, int key) {
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key)
+            count++;
+    }
+
+    return count;
+}
+
+int main() {
+    int n, key;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter element whose frequency is to be found: ";
+    cin >> key;
+
+    cout << "Frequency = " << frequency(arr, n, key);
+
+    return 0;
+}
