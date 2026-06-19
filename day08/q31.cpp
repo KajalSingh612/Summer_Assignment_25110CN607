6c3d2fd2a944af425e6efb77f2749f91eefed1cd
kajal <25110cn607@glbitm.ac.in>
2026-06-19 17:34:09 +0530

    day8 assignment is uploaded

---
+++ day08/q31.cpp
@@ -0,0 +1,17 @@
+//wap to print character triangle pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; ++i) {
+        for (char j = 'A'; j < 'A' + i; ++j) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
